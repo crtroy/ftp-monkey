2e2bc8ccfd231d14fc505009d91cb6738f03e586
hakaboyo <hakaboyo@pdx.edu>
2017-08-14 09:49:10 -0700

    adding list feature

--- src/ftpapp/Controller.java
+++ src/ftpapp/Controller.java
@@ -1,5 +1,7 @@
 package ftpapp;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import model.*;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
@@ -7,8 +9,11 @@ import javafx.scene.control.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 
 import javax.security.auth.login.LoginException;
+import java.io.File;
+import java.io.IOException;
 
 public class Controller {
 
@@ -31,6 +36,18 @@ public class Controller {
     private PasswordField txt_password;
 
     @FXML
+    private ListView<String> view_remote;
+
+    @FXML
+    private ObservableList<String> remoteItems = FXCollections.observableArrayList();
+
+    @FXML
+    private ListView<String> view_local;
+
+    @FXML
+    private ObservableList<String> localItems = FXCollections.observableArrayList();
+
+    @FXML
     private Button btn_disconnect;
 
     public Session session;
@@ -40,12 +57,18 @@ public class Controller {
     @FXML
     private void connectAction(ActionEvent ae) {
         ftp = new FTPClient();
+        System.out.println(System.getProperty("user.home"));
+        //System.out.println(ftp.changeWorkingDirectory(remoteFileDir));
+        //ftp.changeWorkingDirectory(remoteFileDir);
         try {
             session = new Session(ftp, txt_username.getText(), txt_password.getText(), txt_servername.getText(), Integer.parseInt((txt_port.getText())));
 
             if (session.login()) {
                 txt_login_status.setText("Connected!");
                 circle_login_status.setFill(Color.GREEN);
+                remoteFileList();
+                //localFileAndDirecotyList("C:\\Users\\Public");
+                localFileAndDirecotyList(System.getProperty("user.home"));
             }
         }
         catch (Exception e) {
@@ -69,7 +92,7 @@ public class Controller {
 
     @FXML
     private void downloadAction(ActionEvent ae) {
-
+       
     }
 
     @FXML
@@ -82,4 +105,36 @@ public class Controller {
     private void deleteRemoteAction(ActionEvent ae) {
 
     }
+
+    private void remoteFileList()  {
+        try {
+
+            FTPFile [] filesArray = ftp.listFiles();
+
+            for (FTPFile fileItem : filesArray){
+                String info = fileItem.getName();
+                System.out.println(fileItem.getName());
+                remoteItems.add(info);
+            }
+            view_remote.setItems(remoteItems);
+            view_remote.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void localFileAndDirecotyList(String dirNsme){
+        File dirTolist = new File(dirNsme);
+
+        //Get all of the files from a direcory
+        File [] fileArray = dirTolist.listFiles();
+
+        for(File fileObj  :  fileArray){
+            System.out.println(fileObj.getAbsolutePath());
+            localItems.add(fileObj.getAbsolutePath());
+            view_local.setItems(localItems);
+        }
+        view_local.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
+
 }
